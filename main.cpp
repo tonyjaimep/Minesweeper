8f64ad7d2683be928dcd88e7da34e132599fbfb2
tonyjaimep <tonyjaimep@gmail.com>
2019-06-03 23:54:56 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// TODO: Uncomment and build this darn game a GUI
+// #include <SFML/Graphics.hpp>
+
+int main(void) {
+	//	TODO:
+	//	create a board
+	//	game loop (while last popped/revealed cell isn't a bomb)
+	//		pop/reveal cell
+	//	reveal all bombs
+	return 0;
+}
